eda97eb09695b9e0f5455fae0793ee6973e8ca7d
kkawaura <kenkei773@icloud.com>
2021-05-31 10:39:07 +0900

    0531

---
+++ pages/_department/_Production.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <p>印刷物で、まず目をひくのは“美しい文字と鮮明な画像”です。だから私たちは、日本語組版と画像処理技術に拘りを持ち続けます。我が社が担った印刷物は、書店をはじめ、さまざまなところで注目を浴びています。印刷のプロではない他業界の方々からも「大盛印刷が手掛けた印刷物は、他とは違う印象と品質で、とてもきれいだ」というお褒めの言葉をいただきます。なぜ、弊社が手掛けたものが、他社とは違うのでしょう。それは、私たちが美しい日本語組版と画像処理への徹底した拘りを持ち続けているからです。
+    </p>
+    
+    <h3>設備</h3>
+
+    <h4>アプリケーション</h4>
+    <pre>
+      Adobe Illustrator 8.01〜CC2020
+      Adobe InDesign 2.0〜CC2020
+      Adobe Photoshop 5.5〜CC2020
+      QuarkXPress 4.1/6.5/8
+      Edicolor 8.0.2
+
+      和文フォント
+      モリサワパスポート
+      フォントワークスLETS
+      イワタLETS
+      OpenType100 Standard
+    </pre>
+
+    {{ this.$route.params }}
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
